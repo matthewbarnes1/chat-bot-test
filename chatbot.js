3380088edf867db6279bd93b96755233745c3bf7
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-08-24 13:06:43 -0400

    Testing day 2

--- chatbot.js
+++ chatbot.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 // * Creates and stores a wrapper for the OpenAI package along with basic configuration
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
-  temperature: 0,
+  temperature: 0.2,
   model: "gpt-3.5-turbo",
 });
 
